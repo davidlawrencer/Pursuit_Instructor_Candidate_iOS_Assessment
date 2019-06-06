09c9d4142057bcecff8179b459ea3a5f8c81b0e6
davidlawrencer <davidlrifkin+github@gmail.com>
2019-06-06 15:17:00 -0400

    Initial Commit

---
+++ Model/UserModel.swift
@@ -0,0 +1,13 @@
+//
+//  UserModel.swift
+//  Quizzes
+//
+//  Created by Student on 2/1/19.
+//  Copyright © 2019 Alex Paul. All rights reserved.
+//
+
+import Foundation
+struct User: Codable {
+    let imageData: Data
+    let userName: String
+}
